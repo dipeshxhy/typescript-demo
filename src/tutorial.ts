1ae2e80b6cab32f7255a1bc417cc3080dea1e6a2
dipeshxhy <babasarkar260@gmail.comm>
2025-05-16 12:04:53 +0545

    functions , it's default parameters , objects in function params

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -24,85 +24,131 @@
 // console.log(discount);
 
 //Array type
-let temperatures: number[] = [20, 30, 40];
-temperatures.push(50);
+// let temperatures: number[] = [20, 30, 40];
+// temperatures.push(50);
 
 // temperatures.push('20')
 
-let names: string[] = ["dipesh", "karan", "sahil"];
-names.push("d");
+// let names: string[] = ["dipesh", "karan", "sahil"];
+// names.push("d");
 
-let mixed: (number | string)[] = ["dipesh", 20];
-mixed.push("ram");
+// let mixed: (number | string)[] = ["dipesh", 20];
+// mixed.push("ram");
 // mixed.push(true)
 
 //objects
-let car: {
-  brand: string;
-  year: number;
-  isElectric: boolean;
-} = {
-  brand: "BMW",
-  year: 2020,
-  isElectric: true,
-};
-console.log(car.brand);
+// let car: {
+//   brand: string;
+//   year: number;
+//   isElectric: boolean;
+// } = {
+//   brand: "BMW",
+//   year: 2020,
+//   isElectric: true,
+// };
+// console.log(car.brand);
 
-let book: { title: string; cost: number } = {
-  title: "1984",
-  cost: 10,
-};
-let notebook = {
-  title: "notebook",
-};
+// let book: { title: string; cost: number } = {
+//   title: "1984",
+//   cost: 10,
+// };
+// let notebook = {
+//   title: "notebook",
+// };
 
-let pen: { title: string; cost: number } = {
-  title: "pen",
-  cost: 5,
-};
-let stationary: { title: string; cost?: number }[] = [book, notebook, pen];
-console.log(stationary);
+// let pen: { title: string; cost: number } = {
+//   title: "pen",
+//   cost: 5,
+// };
+// let stationary: { title: string; cost?: number }[] = [book, notebook, pen];
+// console.log(stationary);
 
-stationary.push({ title: "pencil", cost: 2 });
-console.log(stationary);
+// stationary.push({ title: "pencil", cost: 2 });
+// console.log(stationary);
 
-const projects: {
-  readonly title: string;
-  description: string;
-  status: string;
-} = {
-  title: "jobify app",
-  description: "jobify app description",
-  status: "active",
-};
+// const projects: {
+//   readonly title: string;
+//   description: string;
+//   status: string;
+// } = {
+//   title: "jobify app",
+//   description: "jobify app description",
+//   status: "active",
+// };
 // projects.title="my app"
-projects.status = "completed";
+// projects.status = "completed";
 
 //challenge 3
 
-const bike: {
-  brand: string;
-  year: number;
-} = {
-  brand: "yamaha",
-  year: 2020,
-};
+// const bike: {
+//   brand: string;
+//   year: number;
+// } = {
+//   brand: "yamaha",
+//   year: 2020,
+// };
 
 // bike.year = '2023';
 
-const laptop: {
-  brand: string;
-  year: number;
-} = {
-  brand: "apple",
-  year: 2020,
+// const laptop: {
+//   brand: string;
+//   year: number;
+// } = {
+//   brand: "apple",
+//   year: 2020,
+// };
+
+// const juice: {
+//   name: string;
+//   cost: number | string;
+// } = {
+//   name: "orange",
+//   cost: 10,
+// };
+// juice.cost = "high price";
+
+//-----function -----
+
+// const names: string[] = ["dipesh", "karan", "sahil", "suman", "sakshi"];
+
+// function nameInLists(name: string): boolean {
+//   return names.includes(name);
+// }
+
+// let checkName = "sudip";
+// if (nameInLists(checkName)) {
+//   console.log(`${checkName} is present in the list`);
+// } else {
+//   console.log(`${checkName} is not present in the list`);
+// }
+
+// const processInput = (input: string | number): void => {
+//   if (typeof input === "number") {
+//     console.log(input * 2);
+//   } else {
+//     console.log(input.toUpperCase());
+//   }
+// };
+
+// processInput("dipesh");
+// processInput(2);
+
+const processedData = (
+  input: string | number,
+  config: { reversed: boolean } = {
+    reversed: false,
+  }
+): string | number => {
+  if (typeof input === "number") {
+    return input ** 2;
+  }
+  if (typeof input === "string" && config.reversed) {
+    return input.split("").reverse().join("").toUpperCase();
+  }
+  return input.toUpperCase();
 };
 
-const juice: {
-  name: string;
-  cost: number | string;
-} = {
-  name: "orange",
-  cost: 10,
-};
-juice.cost = "high price";
+console.log(processedData(2));
+console.log(processedData("dipesh", { reversed: true }));
+console.log(processedData("1234", { reversed: true }));
+console.log(typeof processedData("1234"));
